79ef3471494fd90ba44d4b9cfcff4f9eed9127da
Basinger <fel_simon@mail.ru>
2023-04-22 12:52:59 +0300

    Added Task001_IntegerN

---
+++ Task001_IntegersN/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Задайте значение N. Напишите программу, которая выведет 
+//  все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int NumberN()
+{
+    System.Console.Write("Enter N: ");
+    int N = int.Parse(Console.ReadLine());
+    return N;
+}
+
+void PrintNum(int m)
+{
+if (m >= 1)
+{
+    Console.Write(m + " ");
+    PrintNum(m-1);
+}
+}
+PrintNum(NumberN());
