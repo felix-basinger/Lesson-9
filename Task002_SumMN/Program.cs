05e1c68c5d201dec62ca684c9730eafc7c054c84
Basinger <fel_simon@mail.ru>
2023-04-22 18:28:59 +0300

    Added Task002_SumMN

---
+++ Task002_SumMN/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт сумму 
+//  натуральных элементов в промежутке от M до N.
+
+int GetNum(string arg)
+{
+    System.Console.Write($"Enter {arg}: ");
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+int PrintNum(int m, int n)
+{
+    if (m < n) return PrintNum(m + 1, n) + m;        
+    else return m;
+}
+System.Console.WriteLine($"Sum of int = {PrintNum(GetNum("N"), GetNum("M"))}");  
+
+ 
